bd589c6b48d4f8e611739c17af9a157b255dd513
WalterBright <ssmbuild@outlook.com>
2018-06-22 11:34:01 +0800

    添加浏览器FAB背景虚化

--- app/src/main/java/com/example/ssmbu/gankki/ui/activity/BrowserActivity.java
+++ app/src/main/java/com/example/ssmbu/gankki/ui/activity/BrowserActivity.java
@@ -1,14 +1,15 @@
 package com.example.ssmbu.gankki.ui.activity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -26,6 +27,8 @@ import butterknife.ButterKnife;
 
 public class BrowserActivity extends AppCompatActivity {
     private static final String TAG = "BrowserActivity";
+    @BindView(R.id.floatingback)
+    ImageView floatingback;
     private Intent mIntent;
     @BindView(R.id.share)
     FloatingActionButton share;
@@ -34,15 +37,15 @@ public class BrowserActivity extends AppCompatActivity {
     @BindView(R.id.floatingActionsMenu)
     FloatingActionsMenu floatingActionsMenu;
 
-    private GankItem mGankItem;
-
-    private boolean mStarState=false;
-    private boolean mStarStateOnCreate=false;
-
     @BindView(R.id.webview)
     WebView mWebview;
     @BindView(R.id.progressBar)
     ProgressBar progressBar;
+
+    private GankItem mGankItem;
+
+    private boolean mStarState = false;
+    private boolean mStarStateOnCreate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,11 +54,11 @@ public class BrowserActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         mIntent = getIntent();
-        mGankItem=(GankItem) mIntent.getParcelableExtra("gankItem_data");
+        mGankItem = (GankItem) mIntent.getParcelableExtra("gankItem_data");
         if (mGankItem.getUrl().equals("")) {
             mGankItem.setUrl("http://gank.io/");
         }
-        if(mGankItem.getDesc().equals("")){
+        if (mGankItem.getDesc().equals("")) {
             mGankItem.setDesc("快来看这个干货");
         }
         initStarState();
@@ -65,21 +68,19 @@ public class BrowserActivity extends AppCompatActivity {
     }
 
 
-
-    private void initStarState(){
-        List<GankItem> gankItems=
-        SQLite.select().from(GankItem.class).where(GankItem_Table._id.eq(mGankItem.get_id())).queryList();
-        if(gankItems.size()==0){
+    private void initStarState() {
+        List<GankItem> gankItems =
+                SQLite.select().from(GankItem.class).where(GankItem_Table._id.eq(mGankItem.get_id())).queryList();
+        if (gankItems.size() == 0) {
             //Log.d(TAG, "initStarState: 未收藏");
-            mStarState=false;
+            mStarState = false;
             mStar.setIcon(R.drawable.ic_star_border_white_24dp);
-        }
-        else {
+        } else {
             //Log.d(TAG, "initStarState: 已收藏");
-            mStarState=true;
+            mStarState = true;
             mStar.setIcon(R.drawable.ic_star_white_24dp);
         }
-        mStarStateOnCreate=mStarState;
+        mStarStateOnCreate = mStarState;
     }
 
     private void initView() {
@@ -88,16 +89,14 @@ public class BrowserActivity extends AppCompatActivity {
             //https://blog.csdn.net/qq_36113598/article/details/78175478
             //启动知乎，bilibili等app，防止报错UNKNOWN SCHEME
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url)
-            {
-                if(url == null) return false;
-                Log.d(TAG, "shouldOverrideUrlLoading: "+url);
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url == null) return false;
+                Log.d(TAG, "shouldOverrideUrlLoading: " + url);
                 try {
                     if (url.startsWith("http:") || url.startsWith("https:")) {
                         view.loadUrl(url);
                         return true;
-                    }
-                    else {
+                    } else {
                         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                         startActivity(intent);
                         return true;
@@ -138,8 +137,8 @@ public class BrowserActivity extends AppCompatActivity {
                     public void run() {
                         try {
                             Thread.sleep(500);
-                            floatingActionsMenu.toggle();
-                        }catch (InterruptedException e){
+                            floatingActionsMenu.collapse();
+                        } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
@@ -149,11 +148,51 @@ public class BrowserActivity extends AppCompatActivity {
         share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                floatingActionsMenu.toggle();
+                floatingActionsMenu.collapse();
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_TEXT, mGankItem.getDesc()+"\n"+ mGankItem.getUrl());
+                intent.putExtra(Intent.EXTRA_TEXT, mGankItem.getDesc() + "\n" + mGankItem.getUrl());
                 startActivity(Intent.createChooser(intent, "分享至……"));
+
+            }
+        });
+        //展开FAB显示背景空白，点击空白收起FAB
+        floatingback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floatingActionsMenu.toggle();
+            }
+        });
+
+        floatingActionsMenu.setOnFloatingActionsMenuUpdateListener(new FloatingActionsMenu.OnFloatingActionsMenuUpdateListener() {
+            @Override
+            public void onMenuExpanded() {
+                floatingback.animate()
+                        .setDuration(200)
+                        .alpha(0.7f)
+                        .withStartAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                floatingback.setVisibility(View.VISIBLE);
+                            }
+                        })
+                        .start();
+
+            }
+
+            @Override
+            public void onMenuCollapsed() {
+                floatingback.animate()
+                        .setDuration(200)
+                        .alpha(0f)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                floatingback.setVisibility(View.INVISIBLE);
+                            }
+                        })
+                        .start()
+                ;
 
             }
         });
@@ -171,17 +210,16 @@ public class BrowserActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        if(mStarStateOnCreate&&!mStarState){
+        if (mStarStateOnCreate && !mStarState) {
             //取消收藏
             SQLite.delete()
                     .from(GankItem.class)
                     .where(GankItem_Table._id.eq(mGankItem.get_id()))
                     .query();
-        }
-        else if(!mStarStateOnCreate&&mStarState){
-            boolean save= mGankItem.save();
-            if(!save){
-                Toast.makeText(BrowserActivity.this,"收藏失败啦",Toast.LENGTH_SHORT).show();
+        } else if (!mStarStateOnCreate && mStarState) {
+            boolean save = mGankItem.save();
+            if (!save) {
+                Toast.makeText(BrowserActivity.this, "收藏失败啦", Toast.LENGTH_SHORT).show();
             }
 
         }
@@ -189,20 +227,19 @@ public class BrowserActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    private void toggleStarState(){
-        if(mStarState){
-            mStarState=false;
+    private void toggleStarState() {
+        if (mStarState) {
+            mStarState = false;
             mStar.setTitle("收藏");
             mStar.setIcon(R.drawable.ic_star_border_white_24dp);
-        }
-        else {
-            mStarState=true;
+        } else {
+            mStarState = true;
             mStar.setTitle("已收藏");
             mStar.setIcon(R.drawable.ic_star_white_24dp);
         }
     }
 
-    private int getDBCounts(){
+    private int getDBCounts() {
         return SQLite.select().from(GankItem.class).queryList().size();
     }
 }
